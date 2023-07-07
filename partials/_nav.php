63689f9572616248fd49e8a4a40f429089aac8f2
Akshit <107440152+AkshitGupta2002@users.noreply.github.com>
2023-07-07 11:38:20 +0530

    Add files via upload

---
+++ partials/_nav.php
@@ -0,0 +1,30 @@
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/loginsystem"><b>Sample Login Page</b></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <!-- <li class="nav-item active">
+          <a class="nav-link" href="/loginsystem/login.php">Home <span class="sr-only">(current)</span></a>
+        </li> -->
+
+        <li class="nav-item">
+          <a class="nav-link" href="/loginsystem/login.php">LogIn</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="/loginsystem/signup.php">SignUp</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="/loginsystem/logout.php">LogOut</a>
+        </li>
+
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+       
+      </form>
+    </div>
+  </nav>
